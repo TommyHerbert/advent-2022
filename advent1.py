4cb471c62c59ea222d7da517459638ebd83292bb
Tommy Herbert <tommyherbert@gmail.com>
2023-01-13 23:18:50 +0000

    Duplicate function - planning to adjust the logic for the second part of the problem

--- advent1.py
+++ advent1.py
@@ -8,3 +8,14 @@ def solve(filename):
                 elves[-1] += int(line.strip())
     return max(elves)
 
+
+def solve_part_b(filename):
+    elves = [0]
+    with open(filename) as input:
+        for line in input.readlines():
+            if line == '\n':
+                elves.append(0)
+            else:
+                elves[-1] += int(line.strip())
+    return max(elves)
+
