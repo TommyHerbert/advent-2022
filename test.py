0cda14b845d45a099263fcb7f804f7da1c38583d
Tommy Herbert <tommyherbert@gmail.com>
2023-02-08 16:34:25 +0000

    Test solution for task 1b

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ class TestStringMethods(unittest.TestCase):
     def test_1a(self):
         self.assertEqual(advent1.solve('input/input1.txt'), 66306)
 
+    def test_1b(self):
+        self.assertEqual(advent1.solve_part_b('input/input1.txt'), 195292)
+
 
 if __name__ == '__main__':
     unittest.main()
