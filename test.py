ce4b2927626d979bf1420b33544de3f0d42de879
Tommy Herbert <tommyherbert@gmail.com>
2023-02-08 16:53:57 +0000

    Test for day 2

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-import unittest
-import advent1
+import unittest, advent1, advent2
 
 
 class TestStringMethods(unittest.TestCase):
@@ -8,6 +7,9 @@ class TestStringMethods(unittest.TestCase):
 
     def test_1b(self):
         self.assertEqual(advent1.solve_part_b('input/input1.txt'), 195292)
+
+    def test_2a(self):
+        self.assertEqual(advent2.solve_part_a('input/example2.txt'), 15)
 
 
 if __name__ == '__main__':
