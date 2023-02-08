fd176948ea4a6b749e969fc48d2868d32e717a2c
Tommy Herbert <tommyherbert@gmail.com>
2023-02-08 17:09:34 +0000

    Score lookup table

--- advent2.py
+++ advent2.py
@@ -1,5 +1,11 @@
 import utils
 
+scores = {
+    'A X\n': 4, 'A Y\n': 1, 'A Z\n': 7,
+    'B X\n': 8, 'B Y\n': 5, 'B Z\n': 2,
+    'C X\n': 3, 'C Y\n': 9, 'C Z\n': 6
+}
+
 
 def solve_part_a(path):
     lines = utils.get_lines(path)
