0a91c15df609ebaf6d9df4455f8edd987d1d3363
Tommy Herbert <tommyherbert@gmail.com>
2023-02-08 21:40:54 +0000

    Solution 2b

--- advent2.py
+++ advent2.py
@@ -1,16 +1,22 @@
 import utils
 
-scores = {
+scores_a = {
     'A X\n': 4, 'A Y\n': 8, 'A Z\n': 3,
     'B X\n': 1, 'B Y\n': 5, 'B Z\n': 9,
     'C X\n': 7, 'C Y\n': 2, 'C Z\n': 6
 }
 
+scores_b = {
+    'A X\n': 3, 'A Y\n': 4, 'A Z\n': 8,
+    'B X\n': 1, 'B Y\n': 5, 'B Z\n': 9,
+    'C X\n': 2, 'C Y\n': 6, 'C Z\n': 7
+}
+
 
 def solve_part_a(path):
-    return sum([scores[line] for line in utils.get_lines(path)])
+    return sum([scores_a[line] for line in utils.get_lines(path)])
 
 
 def solve_part_b(path):
-    return 0 # TODO
+    return sum([scores_b[line] for line in utils.get_lines(path)])
 
