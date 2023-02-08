896eca1713c22772a7fb204a7620a8d0e00130ad
Tommy Herbert <tommyherbert@gmail.com>
2023-02-08 16:38:57 +0000

    REFACTOR: Reduce code duplication

--- advent1.py
+++ advent1.py
@@ -1,15 +1,14 @@
 def solve_part_a(filename):
-    elves = [0]
-    with open(filename) as input:
-        for line in input.readlines():
-            if line == '\n':
-                elves.append(0)
-            else:
-                elves[-1] += int(line.strip())
-    return max(elves)
+    return max(get_elves(filename))
 
 
 def solve_part_b(filename):
+    elves = get_elves(filename)
+    elves.sort()
+    return sum(elves[-3:])
+
+
+def get_elves(filename):
     elves = [0]
     with open(filename) as input:
         for line in input.readlines():
@@ -17,6 +16,5 @@ def solve_part_b(filename):
                 elves.append(0)
             else:
                 elves[-1] += int(line.strip())
-    elves.sort()
-    return sum(elves[-3:])
+    return elves
 
