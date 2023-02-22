91b1642c7c37022918d97ce000d0f09ca5d7e670
Tommy Herbert <tommyherbert@gmail.com>
2023-02-22 21:03:01 +0000

    WIP: Day 3 solution

--- day3.py
+++ day3.py
@@ -1,3 +1,24 @@
+import utils
+
+
 def solve_part_a(path):
+    # return sum([get_priority(l) for l in utils.get_lines(path)])
     return 0 # TODO
+
+
+def get_priority(line):
+    halfway = len(line.trim())/2
+    first_half = Set([c for c in line[:halfway]])
+    for c in line[halfway:]:
+        if c in first_half:
+            return value(c)
+    return 0
+
+
+def value(character):
+    if character.islower():
+        # TODO what constants do I need here?
+        return ord(character) - 40
+    else:
+        return ord(character) + 20
 
