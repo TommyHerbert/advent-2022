0d0dcc3b61eb6a0a65d15d0d076acce661f2115a
Tommy Herbert <tommyherbert@gmail.com>
2023-02-23 09:21:38 +0000

    Solution 3b

--- day3.py
+++ day3.py
@@ -1,12 +1,14 @@
-import utils
+from utils import get_lines
+from functools import reduce
 
 
 def solve_part_a(path):
-    return sum([get_priority(l) for l in utils.get_lines(path)])
+    return sum([get_priority(l) for l in get_lines(path)])
 
 
 def solve_part_b(path):
-    return 0 # TODO
+    groups = divide(get_lines(path))
+    return sum([get_badge_priority(group) for group in groups])
 
 
 def get_priority(line):
@@ -23,4 +25,20 @@ def value(character):
         return ord(character) - 96
     else:
         return ord(character) - 38
+
+
+def get_badge_priority(group):
+    sets = [{character for character in line.strip()} for line in group]
+    matches = reduce(lambda s1, s2: s1.intersection(s2), sets)
+    return value(list(matches)[0])
+
+
+def divide(data):
+    threes = []
+    for i in range(len(data)):
+        if i%3 == 0:
+            threes.append([data[i]])
+        else:
+            threes[-1].append(data[i])
+    return threes
 
