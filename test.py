445e188d4ce80d1071a5d5044f6a5d9dcc8a494c
TommyHerbert <tommyherbert@gmail.com>
2023-04-16 20:25:59 +0100

    Use input instead of example file for 7a; swap expected and actual values in tests

--- test.py
+++ test.py
@@ -3,51 +3,51 @@ import unittest, day1, day2, day3, day4, day5, day6, day7
 
 class TestStringMethods(unittest.TestCase):
     def test_day_1(self):
-        self.assertEqual(day1.solve_part_a('input/example1.txt'), 24000)
-        self.assertEqual(day1.solve_part_a('input/input1.txt'), 66306)
-        self.assertEqual(day1.solve_part_b('input/example1.txt'), 45000)
-        self.assertEqual(day1.solve_part_b('input/input1.txt'), 195292)
+        self.assertEqual(24000, day1.solve_part_a('input/example1.txt'))
+        self.assertEqual(66306, day1.solve_part_a('input/input1.txt'))
+        self.assertEqual(45000, day1.solve_part_b('input/example1.txt'))
+        self.assertEqual(195292, day1.solve_part_b('input/input1.txt'))
 
     def test_day_2(self):
-        self.assertEqual(day2.solve_part_a('input/example2.txt'), 15)
-        self.assertEqual(day2.solve_part_a('input/input2.txt'), 15422)
-        self.assertEqual(day2.solve_part_b('input/example2.txt'), 12)
-        self.assertEqual(day2.solve_part_b('input/input2.txt'), 15442)
+        self.assertEqual(15, day2.solve_part_a('input/example2.txt'))
+        self.assertEqual(15422, day2.solve_part_a('input/input2.txt'))
+        self.assertEqual(12, day2.solve_part_b('input/example2.txt'))
+        self.assertEqual(15442, day2.solve_part_b('input/input2.txt'))
 
     def test_day_3(self):
-        self.assertEqual(day3.solve_part_a('input/example3.txt'), 157)
-        self.assertEqual(day3.solve_part_a('input/input3.txt'), 8243)
-        self.assertEqual(day3.solve_part_b('input/example3.txt'), 70)
-        self.assertEqual(day3.solve_part_b('input/input3.txt'), 2631)
+        self.assertEqual(157, day3.solve_part_a('input/example3.txt'))
+        self.assertEqual(8243, day3.solve_part_a('input/input3.txt'))
+        self.assertEqual(70, day3.solve_part_b('input/example3.txt'))
+        self.assertEqual(2631, day3.solve_part_b('input/input3.txt'))
 
     def test_day_4(self):
-        self.assertEqual(day4.solve_part_a('input/example4.txt'), 2)
-        self.assertEqual(day4.solve_part_a('input/input4.txt'), 511)
-        self.assertEqual(day4.solve_part_b('input/example4.txt'), 4)
-        self.assertEqual(day4.solve_part_b('input/input4.txt'), 821)
+        self.assertEqual(2, day4.solve_part_a('input/example4.txt'))
+        self.assertEqual(511, day4.solve_part_a('input/input4.txt'))
+        self.assertEqual(4, day4.solve_part_b('input/example4.txt'))
+        self.assertEqual(821, day4.solve_part_b('input/input4.txt'))
 
     def test_day_5(self):
-        self.assertEqual(day5.solve_part_a('input/example5.txt'), 'CMZ')
-        self.assertEqual(day5.solve_part_a('input/input5.txt'), 'ZRLJGSCTR')
-        self.assertEqual(day5.solve_part_b('input/example5.txt'), 'MCD')
-        self.assertEqual(day5.solve_part_b('input/input5.txt'), 'PRTTGRFPB')
+        self.assertEqual('CMZ', day5.solve_part_a('input/example5.txt'))
+        self.assertEqual('ZRLJGSCTR', day5.solve_part_a('input/input5.txt'))
+        self.assertEqual('MCD', day5.solve_part_b('input/example5.txt'))
+        self.assertEqual('PRTTGRFPB', day5.solve_part_b('input/input5.txt'))
 
     def test_day_6(self):
-        self.assertEqual(day6.solve_part_a('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 'string'), 7)
-        self.assertEqual(day6.solve_part_a('bvwbjplbgvbhsrlpgdmjqwftvncz', 'string'), 5)
-        self.assertEqual(day6.solve_part_a('nppdvjthqldpwncqszvftbrmjlhg', 'string'), 6)
-        self.assertEqual(day6.solve_part_a('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 'string'), 10)
-        self.assertEqual(day6.solve_part_a('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 'string'), 11)
-        self.assertEqual(day6.solve_part_a('input/input6.txt', 'file'), 1757)
-        self.assertEqual(day6.solve_part_b('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 'string'), 19)
-        self.assertEqual(day6.solve_part_b('bvwbjplbgvbhsrlpgdmjqwftvncz', 'string'), 23)
-        self.assertEqual(day6.solve_part_b('nppdvjthqldpwncqszvftbrmjlhg', 'string'), 23)
-        self.assertEqual(day6.solve_part_b('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 'string'), 29)
-        self.assertEqual(day6.solve_part_b('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 'string'), 26)
-        self.assertEqual(day6.solve_part_b('input/input6.txt', 'file'), 2950)
+        self.assertEqual(7, day6.solve_part_a('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 'string'))
+        self.assertEqual(5, day6.solve_part_a('bvwbjplbgvbhsrlpgdmjqwftvncz', 'string'))
+        self.assertEqual(6, day6.solve_part_a('nppdvjthqldpwncqszvftbrmjlhg', 'string'))
+        self.assertEqual(10, day6.solve_part_a('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 'string'))
+        self.assertEqual(11, day6.solve_part_a('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 'string'))
+        self.assertEqual(1757, day6.solve_part_a('input/input6.txt', 'file'))
+        self.assertEqual(19, day6.solve_part_b('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 'string'))
+        self.assertEqual(23, day6.solve_part_b('bvwbjplbgvbhsrlpgdmjqwftvncz', 'string'))
+        self.assertEqual(23, day6.solve_part_b('nppdvjthqldpwncqszvftbrmjlhg', 'string'))
+        self.assertEqual(29, day6.solve_part_b('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 'string'))
+        self.assertEqual(26, day6.solve_part_b('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 'string'))
+        self.assertEqual(2950, day6.solve_part_b('input/input6.txt', 'file'))
 
     def test_day_7(self):
-        self.assertEqual(day7.solve_part_a('input/example7.txt', 100000), 1915606)
+        self.assertEqual(1915606, day7.solve_part_a('input/input7.txt', 100000))
 
 
 if __name__ == '__main__':
