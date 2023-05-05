38e5668982cd64dd711a0947ec691c0ad9d3e8a1
TommyHerbert <tommyherbert@gmail.com>
2023-05-05 15:37:19 +0100

    Add example test for 8b

--- test.py
+++ test.py
@@ -63,6 +63,7 @@ class TestStringMethods(unittest.TestCase):
     def test_day_8(self):
         self.assertEqual(21, day8.solve_part_a('input/example8.txt'))
         self.assertEqual(1870, day8.solve_part_a('input/input8.txt'))
+        self.assertEqual(8, day8.solve_part_b('input/example8.txt'))
 
 
 if __name__ == '__main__':
